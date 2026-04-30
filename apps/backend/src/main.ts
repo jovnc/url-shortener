36fe8c62ea4d03d32c012351c4091c4f3fc934cc
jovnc <95868357+jovnc@users.noreply.github.com>
2026-04-30 23:52:38 +0800

    chore: add @nestjs/swagger

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,21 +2,31 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module.js';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Server Monitor API')
+    .setDescription('The server monitor API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  // Bootstrap configuration
   app.setGlobalPrefix('api/v1', {
     exclude: ['.well-known/(.*)'],
   });
-
-  app.use(cookieParser());
 
   app.enableCors({
     origin: process.env.FRONTEND_URL ?? 'http://localhost:3000',
     credentials: true,
   });
 
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(process.env.PORT ?? 3001);
